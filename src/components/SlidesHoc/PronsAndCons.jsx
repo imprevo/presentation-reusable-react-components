51cb274f7baea7496008f149b1d78b253eafd147
imprevo <zhenek42@gmail.com>
2019-04-17 00:41:30 +0700

    Add HoC cons

--- src/components/SlidesHoc/PronsAndCons.jsx
+++ src/components/SlidesHoc/PronsAndCons.jsx
@@ -11,8 +11,9 @@ const PronsAndCons = () => {
       <Text>Минусы</Text>
       <List>
         <ListItem>Коллизии props</ListItem>
-        <ListItem>Неочевидно, какие props</ListItem>
+        <ListItem>Неочевидно, какие props приходят в компонент</ListItem>
         <ListItem>Нельзя объявлять в рендере/динамически</ListItem>
+        <ListItem>Component hell в react devtools</ListItem>
         <ListItem>
           <s>Сложно типизировать</s> (уже нет - см React.AbstractComponent)
         </ListItem>
